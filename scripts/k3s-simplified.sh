f2225e639a0b2f8cd08b3a8e05a19d3aa1f76407
Maintainer <github@easystartup.io>
2024-01-14 15:14:14 +0800

    Fix run for macos

--- scripts/k3s-simplified.sh
+++ scripts/k3s-simplified.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+JAVA_DIRECTORY=/usr/local/lib/k3s-simplified-java
+JAR_FILE=/usr/local/share/k3s-simplified.jar
+
+# For macos its inside Contents/Home
+if [ "$OS" = "Darwin" ]; then
+    JAVA_DIRECTORY="${JAVA_DIRECTORY}/jdk-21.0.1.jdk/Contents/Home";
+fi
 # Run the JAR file with the newly downloaded or existing Java
-/usr/local/lib/k3s-simplified-java/bin/java -jar "/usr/local/share/k3s-simplified.jar" "$@"
+$JAVA_DIRECTORY/bin/java -jar "${JAR_FILE}" "$@"
